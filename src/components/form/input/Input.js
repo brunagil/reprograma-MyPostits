bc9fd829f4d6da39f5f290d1597c9af0409d434c
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2018-11-13 16:38:25 -0200

    add classes to Input.js and Login.js

--- src/components/form/input/Input.js
+++ src/components/form/input/Input.js
@@ -1,15 +1,73 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './Input.css'
 
-function Input(props) {
-    return (
-        <input
-            type={props.type}
-            id={props.id}
-            className='input'
-            placeholder={props.placeholder}
-        />
-    )
+// EXEMPLO COM O USO DE FUNÇÃO PARA A CONSTRUÇÃO DO INPUT
+// function Input(props) {
+//     return (
+//         <input
+//             onChange={}                   
+//             type={props.type}  //componentes funcionais
+//             id={props.id}
+//             className='input'
+//             placeholder={props.placeholder}
+//         />
+//     )
+// }
+
+//EXEMPLO - utilizando os componentes e classes do React
+class Input extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={message : null} //estado natural do código quando a página carrega 
+    //this.handleChange = this.handleChange.bind(this) // se for utilizado a arrow function, n precisa dessa função
+    
+    }
+    hasError = () => {
+        if(this.state.message === null || this.state.message !== '') {
+            return false 
+        } else {
+            return true
+        }
+    }
+    handleChange = (e) => { //gaurdar a função para ser usada com o this lá embaixo
+        const value = e.target.value
+        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        //console.log('hello onchange', value)
+        let message = ''
+        
+        if(this.props.required && value.trim() === '') { //tirar os espaços em branco e ter a tag 'required' no dom/html
+            message = 'Campo Obrigatório' //mensagem que vai aparecer
+        } else if(this.props.minLength && value.length < this.props.minLength ) {
+            message = `Digite pelo menos ${this.props.minLength} caracteres`
+        } else if(this.props.type ==='email' && !regex.test(value)) { //testa a função regex, e se for falso, dá uma mensagem de erro
+            message= 'Digite um e-mail válido'
+            
+        }
+
+        this.setState({message : message}, this.props.onChange()) //callback  
+        //setState é função do react ==>> situação/estado atual das camisetas. Pode ter erro, pode não ter erro, etc
+        //props ==> características da camiseta 
+
+        //classe ==> muda o estado e function ==> não muda o estado
+
+    }
+
+    render() {
+        return ( //react.fragment pede que os elementos não fiquem jogados, então é específico do React
+            <React.Fragment> 
+                <input
+                    onChange={this.handleChange}
+                    onBlur={this.handleChange}                  
+                    type={this.props.type}  //componentes funcionais
+                    id={this.props.id}
+                    className='input'
+                    placeholder={this.props.placeholder}
+                    required={this.props.required}
+                />
+                <p className='input__erro'>{this.state.message}</p>
+            </React.Fragment>
+        )
+    }
 }
 
 export default Input 
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -2,19 +2,45 @@ import React from 'react'
 import Form from '../../components/form'
 import Container from '../../components/container/container'
 
-function Login() {
-  return(
+// function Login() {
+//   return(
+//     <Container>
+//       <Form title='Login' text='Entre com seu Email e Senha'>
+//           <Form.Label htmlFor='email'>Email:</Form.Label>
+//           <Form.Input id='email' type='email' required></Form.Input>
+//           <Form.Label>Senha:</Form.Label>
+//           <Form.Input type='password' id='passaword' minLength={6} required></Form.Input>
+//           <Form.Button disabled type='submit' className='button'>Enviar</Form.Button>
+//           <Form.Link>Criar uma conta</Form.Link>
+//       </Form>
+//     </Container>
+//   )
+// }
+
+class Login extends React.Component {
+  constructor(){
+    super() 
+    this.state= { disabled : true}
+  } 
+  onDisabledButton = () => {
+    console.log(this.onDisabledButton, 'onDisabledButton')
+
+  }
+  render() {
+    return (
     <Container>
       <Form title='Login' text='Entre com seu Email e Senha'>
           <Form.Label htmlFor='email'>Email:</Form.Label>
-          <Form.Input id='email' type='text'></Form.Input>
+          <Form.Input id='email' type='email' onChange={this.onDisabledButton} required></Form.Input>
           <Form.Label>Senha:</Form.Label>
-          <Form.Input type='password' id='passaword'></Form.Input>
-          <Form.Button disabled type='submit' className='button'>Enviar</Form.Button>
+          <Form.Input type='password' id='passaword' minLength={6} onChange={() => console.log('olá')} required></Form.Input>
+          <Form.Button disabled={this.state.disabled} type='submit' className='button'>Enviar</Form.Button>
           <Form.Link>Criar uma conta</Form.Link>
       </Form>
     </Container>
-  )
+    )
+  }
 }
+
 
 export default Login 
