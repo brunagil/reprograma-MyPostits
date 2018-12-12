bbe0d4ce82bfb28f0197e7db31b78ac7fdc0b102
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2018-12-12 19:11:20 -0200

    fixed Alert error installing material ui

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import Button from '@material-ui/core/Button'
-
 import './Alert.css'
 
 function Alert(props) {
